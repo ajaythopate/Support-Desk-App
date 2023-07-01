715b65a0c9ff5cf11b694a2ed7e23046bd662ebe
ajaythopate <ajaythopate999@gmail.com>
2023-07-01 12:37:58 +0530

    route added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar  from 'react-bootstrap/Navbar';
+import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import { BrowserRouter , Routes,Route } from 'react-router-dom';
+import AdminPages from "./pages/AdminPages";
+import HomePages from "./pages/HomePages";
 
 
 
 
 function App() {
   return (
-    <div className="d-flex flex-column min-vh-100">
-      <header>
-        <Navbar bg="dark" variant="dark" expand="lg">
-          <Container>
-            <Navbar.Brand>Support Desk</Navbar.Brand>
-            <Navbar.Toggle area-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto w-100 justify-content-end">
-                <a href="/admin" className="nav-link" target="_blank">Admin</a>
-              </Nav>
-            </Navbar.Collapse>
+    <BrowserRouter>
+
+      <div className="d-flex flex-column min-vh-100">
+        <header>
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+              <Navbar.Brand>Support Desk</Navbar.Brand>
+              <Navbar.Toggle area-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto w-100 justify-content-end">
+                  <a href="/admin" className="nav-link" target="_blank">Admin</a>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/admin" element={<AdminPages />} ></Route>
+              <Route path="/" element={<HomePages />} />
+
+              </Routes>
           </Container>
-        </Navbar>
-      </header>
-      <main>main</main>
-      <footer className="mt-auto">
-        <div className="text-center">All Rights resrved</div>
-      </footer>
-    </div>
+        </main>
+        <footer className="mt-auto">
+          <div className="text-center">All Rights resrved</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;

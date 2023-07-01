925771a97410a1a9f6c5958708852ed67ea55346
ajaythopate <ajaythopate999@gmail.com>
2023-07-01 14:30:37 +0530

    chatbox create

--- src/pages/HomePages.js
+++ src/pages/HomePages.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import ChatBox from '../components/ChatBox'
 
 export default function HomePages() {
   return (
-    <div>HomePages</div>
+    <div>
+        <h1>HomePage</h1>
+
+          <p>You Website content</p>
+          <ChatBox />
+    </div>
   )
 }

4a06d8951e71ce22289badb8350fdf990b5f6de1
ajaythopate <ajaythopate999@gmail.com>
2023-07-02 19:44:01 +0530

    complete chat box

--- frontend/src/components/ChatBox.js
+++ frontend/src/components/ChatBox.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import socketIOClient from "socket.io-client";
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -10,15 +11,51 @@ import FormControl from 'react-bootstrap/FormControl';
 
 
 
-
+const ENDPOINT =
+    window.location.host.indexOf("localhost") >= 0
+        ? "http://127.0.0.1:4000"
+        : window.location.host;
 
 export default function ChatBox() {
+
+    const uiMessagesRef = useRef(null);
+
+    const [userName, setUserName] = useState("");
+    const [messages, setMessages] = useState([
+        { from: "System", body: "Hello there, please ask your question." },
+    ]);
+
+    const [socket, setSocket] = useState(null);
     const [isOpen, setIsOpen] = useState(false);
     const [messageBody, setMessageBody] = useState('');
+
+
+    useEffect(() => {
+        if (uiMessagesRef.current) {
+            uiMessagesRef.current.scrollBy({
+                top: uiMessagesRef.current.scrollHeight,
+                left: 0,
+                behavior: "smooth",
+            });
+        }
+        if (socket) {
+            socket.emit("onLogin", { name: userName });
+            socket.on("message", (data) => {
+                setMessages([...messages, data]);
+            });
+        }
+
+    }, [messages, socket, userName]);
+
 
     const supportHandler = () => {
 
         setIsOpen(true);
+        if (!userName) {
+            setUserName(prompt("please enter your name"));
+        }
+        const sk = socketIOClient(ENDPOINT);
+        setSocket(sk);
 
     };
     const closeHandler = () => {
@@ -29,7 +66,18 @@ export default function ChatBox() {
         if (!messageBody.trim()) {
             alert("Error . please type message.");
         } else {
-
+            setMessages([
+                ...messages,
+                { body: messageBody, from: userName, to: "Admin" },
+            ]);
+            setTimeout(() => {
+                socket.emit("onMessage", {
+                    body: messageBody,
+                    from: userName,
+                    to: "Admin",
+                });
+            }, 1000);
+            setMessageBody("");
         }
     };
 
@@ -51,8 +99,12 @@ export default function ChatBox() {
                                 </Col>
                             </Row>
                             <hr />
-                            <ListGroup>
-                                <ListGroup.Item>no messages</ListGroup.Item>
+                            <ListGroup ref={uiMessagesRef}>
+                                {
+                                    messages.map((msg, index) => (<ListGroup.Item key={index}>
+                                        <strong>{`${msg.from}: `}</strong>{msg.body}
+                                    </ListGroup.Item>))
+                                }
 
                             </ListGroup>
                             <form onSubmit={submitHandler}>
